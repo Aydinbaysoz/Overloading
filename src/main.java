0157e116cbcbff3c4d6138b6b8411199e26f7b11
Aydin Baysoz <baysozaydin@gmail.com>
2021-10-08 09:38:45 -0400

    Initial commit overloading successfully

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Transaction transaction = new Transaction();
+        System.out.println(transaction.sum(10,20,30));
+        System.out.println(transaction.sum(40,25));
+
+    }
+}
